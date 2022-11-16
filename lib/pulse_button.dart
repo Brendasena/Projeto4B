9623817cf81deb29f88a34ca829aafb35caa2650
projetoscintia <cpinho@tecnologia-unica.com.br>
2022-11-16 08:47:52 -0300

    Primeira versão do app

---
+++ lib/pulse_button.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class PulseButton extends StatelessWidget {
+  const PulseButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
